4955f9cac23c9c9d7c6c3862a90258b92356a676
Alan Stewart <alan@atomist.com>
2016-12-02 15:31:24 +1100

    Only replace colons in Windows paths

--- src/main/scala/com/atomist/util/PathUtils.scala
+++ src/main/scala/com/atomist/util/PathUtils.scala
@@ -5,6 +5,8 @@ import java.util.regex.Matcher
 
 object PathUtils {
 
+  private val IsWindows = System.getProperty("os.name").contains("indows")
+
   private val fileSeparator = Matcher.quoteReplacement(File.separator)
 
   def splitPath(path: String): Array[String] = convertPath(path).split(fileSeparator)
@@ -12,9 +14,10 @@ object PathUtils {
   def convertPaths(paths: Seq[String]) = paths.map(convertPath(_))
 
   def convertPath(path: String) = {
-    if (path == null || path.equals(""))
-      path
-    else
-      path.replaceAll("\\\\|/", fileSeparator).replace(":", "_")
+    if (path == null || path.equals("")) path
+    else {
+      val convertedPath = path.replaceAll("\\\\|/", fileSeparator)
+      if (IsWindows) convertedPath.replace(":", "_") else convertedPath
+    }
   }
 }
