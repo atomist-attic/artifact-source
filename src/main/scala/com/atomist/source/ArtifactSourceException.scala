caa90432a15dd5396531127ec4d682cef6b45e22
Alan Stewart <alankstewart@gmail.com>
2017-03-09 20:16:23 +1100

    Simplify exception

--- src/main/scala/com/atomist/source/ArtifactSourceException.scala
+++ src/main/scala/com/atomist/source/ArtifactSourceException.scala
@@ -1,64 +1,13 @@
 package com.atomist.source
 
 abstract class ArtifactSourceException(message: String, cause: Throwable)
-  extends Exception(message, cause) {
-
-  protected var statusCode: Int = _
-
-  // For Java callers
-  def getStatusCode = statusCode
-}
+  extends Exception(message, cause)
 
 case class ArtifactSourceAccessException(message: String, cause: Throwable)
-  extends ArtifactSourceException(message, cause) {
-
-  def this(statusCode: Int, message: String) = {
-    this(message, null)
-    this.statusCode = statusCode
-  }
-
-  def this(message: String) = this(message, null)
-}
-
-object ArtifactSourceAccessException {
-
-  def apply(statusCode: Int, message: String) = new ArtifactSourceAccessException(statusCode, message)
-
-  def apply(message: String) = new ArtifactSourceAccessException(message)
-}
+  extends ArtifactSourceException(message, cause)
 
 case class ArtifactSourceUpdateException(message: String, cause: Throwable)
-  extends ArtifactSourceException(message, cause) {
-
-  def this(statusCode: Int, message: String) = {
-    this(message, null)
-    this.statusCode = statusCode
-  }
-
-  def this(message: String) = this(message, null)
-}
-
-object ArtifactSourceUpdateException {
-
-  def apply(statusCode: Int, message: String) = new ArtifactSourceUpdateException(statusCode, message)
-
-  def apply(message: String) = new ArtifactSourceUpdateException(message)
-}
+  extends ArtifactSourceException(message, cause)
 
 case class ArtifactSourceCreationException(message: String, cause: Throwable)
-  extends ArtifactSourceException(message, cause) {
-
-  def this(statusCode: Int, message: String) = {
-    this(message, null)
-    this.statusCode = statusCode
-  }
-
-  def this(message: String) = this(message, null)
-}
-
-object ArtifactSourceCreationException {
-
-  def apply(statusCode: Int, message: String) = new ArtifactSourceCreationException(statusCode, message)
-
-  def apply(message: String) = new ArtifactSourceCreationException(message)
-}
+  extends ArtifactSourceException(message, cause)
