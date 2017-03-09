6702ce4145465be2c1e73da866fa4f7d2c988157
Alan Stewart <alan@atomist.com>
2017-03-09 20:35:18 +1100

    Fixed build

--- src/main/scala/com/atomist/source/ArtifactSourceException.scala
+++ src/main/scala/com/atomist/source/ArtifactSourceException.scala
@@ -4,10 +4,34 @@ abstract class ArtifactSourceException(message: String, cause: Throwable)
   extends Exception(message, cause)
 
 case class ArtifactSourceAccessException(message: String, cause: Throwable)
-  extends ArtifactSourceException(message, cause)
+  extends ArtifactSourceException(message, cause) {
+
+  def this(message: String) = this(message, null)
+}
+
+object ArtifactSourceAccessException {
+
+  def apply(message: String) = new ArtifactSourceAccessException(message)
+}
 
 case class ArtifactSourceUpdateException(message: String, cause: Throwable)
-  extends ArtifactSourceException(message, cause)
+  extends ArtifactSourceException(message, cause) {
+
+  def this(message: String) = this(message, null)
+}
+
+object ArtifactSourceUpdateException {
+
+  def apply(message: String) = new ArtifactSourceUpdateException(message)
+}
 
 case class ArtifactSourceCreationException(message: String, cause: Throwable)
-  extends ArtifactSourceException(message, cause)
+  extends ArtifactSourceException(message, cause) {
+
+  def this(message: String) = this(message, null)
+}
+
+object ArtifactSourceCreationException {
+
+  def apply(message: String) = new ArtifactSourceCreationException(message)
+}
--- src/test/scala/com/atomist/source/ArtifactSourceExceptionTest.scala
+++ src/test/scala/com/atomist/source/ArtifactSourceExceptionTest.scala
@@ -6,14 +6,12 @@ class ArtifactSourceExceptionTest extends FlatSpec with Matchers {
 
   it should "create new ArtifactSourceAccessException with message" in {
     val e = new ArtifactSourceAccessException("message")
-    e.getStatusCode should equal(0)
     e.getMessage should equal("message")
     e.getCause should be(null)
   }
 
   it should "create new ArtifactSourceCreationException with statusCode and message" in {
-    val e = new ArtifactSourceCreationException(200, "message")
-    e.getStatusCode should equal(200)
+    val e = new ArtifactSourceCreationException("message")
     e.getMessage should equal("message")
     e.getCause should be(null)
   }
@@ -21,7 +19,6 @@ class ArtifactSourceExceptionTest extends FlatSpec with Matchers {
   it should "create new ArtifactSourceUpdateException with message and throwable" in {
     val iae = new IllegalArgumentException("illegal argument")
     val e = new ArtifactSourceUpdateException("message", iae)
-    e.getStatusCode should equal(0)
     e.getMessage should equal("message")
     e.getCause should not be null
     e.getCause.getMessage should equal("illegal argument")
