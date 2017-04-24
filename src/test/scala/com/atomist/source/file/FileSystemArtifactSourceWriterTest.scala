e61e979230e38a93098996eb15f352b21b85447e
Alan Stewart <alankstewart@gmail.com>
2017-04-24 19:47:24 +1000

    Initial commit to fix bug in path elements

--- src/test/scala/com/atomist/source/file/FileSystemArtifactSourceWriterTest.scala
+++ src/test/scala/com/atomist/source/file/FileSystemArtifactSourceWriterTest.scala
@@ -4,45 +4,53 @@ import java.io.File
 import java.nio.file.{Files, Path}
 
 import com.atomist.source.{ArtifactSourceCreationException, SimpleSourceUpdateInfo, TestUtils}
+import org.apache.commons.io.FileUtils
 import org.scalatest.{FlatSpec, Matchers}
 
 class FileSystemArtifactSourceWriterTest extends FlatSpec with Matchers {
 
-  val helloSource = ClassPathArtifactSource.toArtifactSource("java-source/HelloWorldService.java")
-
-  val validSource = ClassPathArtifactSource.toArtifactSource("spring-boot")
+  import FileSystemArtifactSourceWriterTest._
 
   private def newTemporaryDir: Path = Files.createTempDirectory(getClass.getSimpleName)
 
   val fWriter = new FileSystemArtifactSourceWriter
 
   "FileSystemArtifactSourceWriter" should "fail with bogus file id" in {
-    validSource.empty shouldBe false
+    ValidSource.empty shouldBe false
     val fid = FileSystemArtifactSourceIdentifier(new File(""))
-    an[ArtifactSourceCreationException] should be thrownBy fWriter.write(validSource, fid, SimpleSourceUpdateInfo(getClass.getName))
+    an[ArtifactSourceCreationException] should be thrownBy fWriter.write(ValidSource, fid, SimpleSourceUpdateInfo(getClass.getName))
   }
 
   it should "not allow writing to same id" in {
-    val fid = helloSource.id.asInstanceOf[FileSystemArtifactSourceIdentifier]
-    an[ArtifactSourceCreationException] should be thrownBy fWriter.write(validSource, fid, SimpleSourceUpdateInfo(getClass.getName))
+    val fid = HelloSource.id.asInstanceOf[FileSystemArtifactSourceIdentifier]
+    an[ArtifactSourceCreationException] should be thrownBy fWriter.write(ValidSource, fid, SimpleSourceUpdateInfo(getClass.getName))
   }
 
   it should "produce result with equivalent contents" in {
-    validSource.empty shouldBe false
+    ValidSource.empty shouldBe false
     val fid = FileSystemArtifactSourceIdentifier(newTemporaryDir.toFile)
-    fWriter.write(validSource, fid, SimpleSourceUpdateInfo(getClass.getName))
+    fWriter.write(ValidSource, fid, SimpleSourceUpdateInfo(getClass.getName))
     val as = FileSystemArtifactSource(fid)
     as.artifacts.nonEmpty shouldBe true
-    as.artifacts.size shouldEqual validSource.artifacts.size
-    TestUtils.verify(validSource, as)
+    as.artifacts.size shouldEqual ValidSource.artifacts.size
+    TestUtils.verify(ValidSource, as)
+    FileUtils.deleteQuietly(newTemporaryDir.toFile)
   }
 
   it should "produce files with equivalent contents when read in again" in {
-    validSource.empty shouldBe false
+    ValidSource.empty shouldBe false
     val fid = FileSystemArtifactSourceIdentifier(newTemporaryDir.toFile)
-    fWriter.write(validSource, fid, SimpleSourceUpdateInfo(getClass.getName))
+    fWriter.write(ValidSource, fid, SimpleSourceUpdateInfo(getClass.getName))
     val as = FileSystemArtifactSource(fid)
-    as.artifacts.size shouldEqual validSource.artifacts.size
-    TestUtils.verify(validSource, as)
+    as.artifacts.size shouldEqual ValidSource.artifacts.size
+    TestUtils.verify(ValidSource, as)
+    FileUtils.deleteQuietly(newTemporaryDir.toFile)
   }
+}
+
+object FileSystemArtifactSourceWriterTest {
+
+  private val HelloSource = ClassPathArtifactSource.toArtifactSource("java-source/HelloWorldService.java")
+
+  private val ValidSource = ClassPathArtifactSource.toArtifactSource("spring-boot")
 }
