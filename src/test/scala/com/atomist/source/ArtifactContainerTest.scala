d1d6a74463274c7b3723cf82bcab538eb9f1ff92
James Carnegie <james@atomist.com>
2017-04-24 23:05:43 +0100

    Failing test for atomist/rug#523

---
+++ src/test/scala/com/atomist/source/ArtifactContainerTest.scala
@@ -0,0 +1,17 @@
+package com.atomist.source
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ArtifactContainerTest extends FlatSpec with Matchers{
+
+  it should "find files in /" in {
+    val src = SimpleFileBasedArtifactSource(
+      StringFileArtifact("Foo.java",
+        """
+          |public class Foo {
+          |}
+        """.stripMargin)
+    )
+    assert(src.findFile("/Foo.java").nonEmpty)
+  }
+}
