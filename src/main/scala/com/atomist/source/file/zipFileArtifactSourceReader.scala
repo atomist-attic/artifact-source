1440cc2cf1b510c664d7be046dbf536907b34977
Alan Stewart <alankstewart@gmail.com>
2017-05-09 09:02:05 +1000

    Use Scala concurrency to improve performance (#28)
    * Use Scala concurrency to improve performance
    
    * Decrease atMost value to 15s
    
    * Removed commented code, other cleanups
    
    * Simplified method signature of createFile
    
    * Improved error handling
    
    * Close zip file when exception is thrown
    
    * Polishing

--- src/main/scala/com/atomist/source/file/zipFileArtifactSourceReader.scala
+++ src/main/scala/com/atomist/source/file/zipFileArtifactSourceReader.scala
@@ -1,17 +1,20 @@
 package com.atomist.source.file
 
 import java.io.{File, IOException, InputStream}
-import java.nio.file.attribute.{FileAttribute, PosixFilePermission, PosixFilePermissions}
+import java.nio.file.attribute.{PosixFilePermission, PosixFilePermissions}
 import java.nio.file.{Files, Path, Paths}
 import java.util.{Set => JSet}
 
 import com.atomist.source._
 import com.atomist.util.FilePermissions.fromMode
+import com.atomist.util.Utils.withCloseable
 import org.apache.commons.compress.archivers.zip.{AsiExtraField, ZipArchiveEntry, ZipFile}
 import org.apache.commons.io.FileUtils
 
-import scala.annotation.tailrec
 import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 
 case class ZipFileInput(is: InputStream) extends ArtifactSourceIdentifier {
@@ -37,89 +40,99 @@ object ZipFileArtifactSourceReader {
   private val IsWindows = System.getProperty("os.name").contains("indows")
 
   def fromZipSource(id: ZipFileInput): ArtifactSource = {
-    val tmpFile = File.createTempFile("tmp", ".zip")
-    tmpFile.deleteOnExit()
+    val zipFile = getZipFile(id)
+    val targetFolder = createTargetFolder
+    withCloseable(zipFile)(zf =>
+      Await.ready(Future.sequence(processZipEntries(zf, targetFolder)), Duration(60, SECONDS)).onComplete {
+        case Success(_) =>
+        case Failure(e) => throw ArtifactSourceCreationException(s"Failed to create artifact source", e)
+      })
+
+    val fid = NamedFileSystemArtifactSourceIdentifier(substringAfterLast(targetFolder.getName, "/"), targetFolder)
+    FileSystemArtifactSource(fid)
+  }
+
+  private def getZipFile(id: ZipFileInput) = {
     try {
+      val tmpFile = File.createTempFile("tmp", ".zip")
+      tmpFile.deleteOnExit()
       FileUtils.copyInputStreamToFile(id.is, tmpFile)
-    } catch {
-      case e: IOException => throw ArtifactSourceAccessException(s"Failed to copy zip contents to temp file: ${e.getMessage}", e)
-    }
-
-    val rootFile = Files.createTempDirectory("tmp").toFile
-    rootFile.deleteOnExit()
-
-    val zipFile = try {
       new ZipFile(tmpFile)
     } catch {
-      case e: IOException => throw ArtifactSourceAccessException(s"Expected zip entries in $id but none was found", e)
+      case e: IOException => throw ArtifactSourceCreationException("Failed to copy zip contents to temp file", e)
     }
-
-    unzipFile(zipFile.getEntries.asScala.toList, zipEntryInputStream(zipFile), rootFile)
-    ZipFile.closeQuietly(zipFile)
-    FileSystemArtifactSource(FileSystemArtifactSourceIdentifier(rootFile))
   }
+
+  private def createTargetFolder = {
+    try {
+      val targetFolder = Files.createTempDirectory("artifact-source-").toFile
+      targetFolder.deleteOnExit()
+      targetFolder
+    } catch {
+      case e: IOException => throw ArtifactSourceCreationException("Failed to create folder for artifact source", e)
+    }
+  }
+
+  private def processZipEntries(zipFile: ZipFile, targetFolder: File) =
+    for (entry <- zipFile.getEntries.asScala.toList) yield Future {
+      writeZipEntryToFile(entry, zipEntryInputStream(zipFile), targetFolder)
+    }
 
   private def zipEntryInputStream(zipFile: ZipFile)(entry: ZipArchiveEntry) = zipFile.getInputStream(entry)
 
-  private def unzipFile(entryList: List[ZipArchiveEntry],
-                        is: (ZipArchiveEntry) => InputStream,
-                        targetFolder: File): Unit = {
-    @tailrec
-    def unzipEntries(entryList: List[ZipArchiveEntry],
-                     is: (ZipArchiveEntry) => InputStream,
-                     targetFolder: File): Unit =
-      entryList match {
-        case entry :: entries =>
-          val pathName = entry.getName
-          val path = if (IsWindows) pathName.replace(":", "_") else pathName
-          val file = Paths.get(path).toFile
-          val newPath = Paths.get(targetFolder.getPath, file.getPath)
+  private def writeZipEntryToFile(entry: ZipArchiveEntry,
+                                  is: (ZipArchiveEntry) => InputStream,
+                                  targetFolder: File): Unit = {
+    val pathName = entry.getName
+    val path = if (IsWindows) pathName.replace(":", "_") else pathName
+    val file = Paths.get(path).toFile
+    val newPath = Paths.get(targetFolder.getPath, file.getPath)
 
-          if (file.isDirectory || entry.isDirectory || entry.isUnixSymlink)
-            Files.createDirectories(newPath)
-          else {
-            val parentDir = newPath.toFile.getParentFile.toPath
-            if (!Files.exists(parentDir))
-              parentDir.toFile.mkdirs
+    if (file.isDirectory || entry.isDirectory || entry.isUnixSymlink)
+      Files.createDirectories(newPath)
+    else {
+      val parentDir = newPath.toFile.getParentFile.toPath
+      if (!Files.exists(parentDir))
+        parentDir.toFile.mkdirs
 
-            val perms = getPermissions(entry)
-            val fileAttributes = PosixFilePermissions.asFileAttribute(perms)
-            val newFile = createFile(newPath, perms, fileAttributes)
-            FileUtils.copyInputStreamToFile(is(entry), newFile)
-          }
+      val perms = getPermissions(entry)
+      val newFile = createFile(newPath, perms)
+      FileUtils.copyInputStreamToFile(is(entry), newFile)
+    }
+  }
 
-          unzipEntries(entries, is, targetFolder)
-        case _ =>
-      }
+  private def getPermissions(entry: ZipArchiveEntry): JSet[PosixFilePermission] = {
+    val unixMode = entry.getUnixMode
+    val mode = unixMode match {
+      case 0 =>
+        entry.getExtraFields().collectFirst {
+          case aef: AsiExtraField => aef.getMode
+          case _ => FileArtifact.DefaultMode
+        }.getOrElse(FileArtifact.DefaultMode)
+      case _ => unixMode
+    }
+    fromMode(mode)
+  }
 
-    def createFile(newPath: Path,
-                   perms: JSet[PosixFilePermission],
-                   fileAttributes: FileAttribute[JSet[PosixFilePermission]]) =
-      Try(Files.createFile(newPath, fileAttributes).toFile) match {
-        case Success(f) => f
-        case Failure(_: UnsupportedOperationException) =>
-          // In case of windows
-          val f = Files.createFile(newPath).toFile
-          f.setExecutable(perms contains PosixFilePermission.OWNER_EXECUTE)
-          f.setWritable(perms contains PosixFilePermission.OWNER_WRITE)
-          f
-        case Failure(t: Throwable) =>
-          throw ArtifactSourceCreationException(s"Failed to create file '${newPath.toString}'", t)
-      }
-
-    def getPermissions(entry: ZipArchiveEntry): JSet[PosixFilePermission] = {
-      val unixMode = entry.getUnixMode
-      val mode = unixMode match {
-        case 0 =>
-          entry.getExtraFields().collectFirst {
-            case aef: AsiExtraField => aef.getMode
-            case _ => FileArtifact.DefaultMode
-          }.getOrElse(FileArtifact.DefaultMode)
-        case _ => unixMode
-      }
-      fromMode(mode)
+  private def createFile(newPath: Path, perms: JSet[PosixFilePermission]) =
+    Try {
+      val fileAttributes = PosixFilePermissions.asFileAttribute(perms)
+      Files.createFile(newPath, fileAttributes).toFile
+    } match {
+      case Success(f) => f
+      case Failure(_: UnsupportedOperationException) =>
+        // In case of Windows
+        val f = Files.createFile(newPath).toFile
+        f.setExecutable(perms contains PosixFilePermission.OWNER_EXECUTE)
+        f.setWritable(perms contains PosixFilePermission.OWNER_WRITE)
+        f
+      case Failure(t: Throwable) =>
+        throw t
     }
 
-    unzipEntries(entryList, is, targetFolder)
-  }
+  private def substringAfterLast(s: String, k: String) =
+    s.lastIndexOf(k) match {
+      case -1 => s;
+      case i => s.substring(i + s.length)
+    }
 }
